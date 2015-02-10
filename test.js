0520f8d2479c60bbb41b3897421370e57faa77c1
tunnckoCore <mameto_100@mail.bg>
2015-02-11 00:16:30 +0200

    add more tests

--- test.js
+++ test.js
@@ -9,9 +9,18 @@
 
 var assert = require('assert');
 var glob2fp = require('./index');
+var parsePath = require('parse-filepath');
 
 describe('glob2fp:', function() {
-  it('should accept arrays', function(done) {
+  it('should accept array of glob patterns', function(done) {
+    var actual = glob2fp([
+      'a/b/c/???/e/*.*',
+      'a/b/c/{foo,bar}/e/*',
+      '!a/b/c/foo/e/*.js',
+      'a/b/**.{js,md}',
+      'a/{b,{c,foo},e/d}/*.{js,md,txt}',
+      'a/b/{c..e}/*.{js,md,txt}'
+    ]);
     var expected = [
       'a/b/c',
       'a/b/c',
@@ -20,15 +29,33 @@ describe('glob2fp:', function() {
       'a',
       'a/b'
     ];
-    var actual = glob2fp([
-      'a/b/c/???/e/*.*',
-      'a/b/c/{foo,bar}/e/*',
-      'a/b/c/foo/e/*.js',
-      'a/b/**.{js,md}',
-      'a/{b,{c,foo},e/d}/*.{js,md,txt}',
-      'a/b/{c..e}/*.{js,md,txt}'
-    ]);
+
     assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should accept string glob pattern', function(done) {
+    var actual = glob2fp('a/b/c/???/e/*.*');
+    var expected = 'a/b/c';
+
+    assert.strictEqual(actual, expected);
+    done();
+  });
+  it('should return empty string when no arguments', function(done) {
+    var actual = glob2fp();
+    var expected = '';
+
+    assert.strictEqual(actual, expected);
+    done();
+  });
+  it('should support options.criteria', function(done) {
+    var actual = glob2fp('a/r/s/w/q/{b,{c,foo},e/d}/*.{js,md,txt}', {
+      criteria: function(fp) {
+        return parsePath(fp).dirname
+      }
+    });
+    var expected = 'a/r/s/w/q';
+
+    assert.strictEqual(actual, expected);
     done();
   });
 });
